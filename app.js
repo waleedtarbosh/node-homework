e886e6e9886396083907f2b67fb6c52f9a6d7752
waleedtarbosh <waleedtarbush@gmail.com>
2026-07-12 08:36:16 -0400

    Add Express app.js for Core Task 4

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.post("/testpost", (req, res) => {
+  res.status(200).json({
+    message: "POST route works",
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`);
+});
+
+module.exports = { app, server };
