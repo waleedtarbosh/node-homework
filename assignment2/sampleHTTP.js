e5a1a65727a3f48677f0586148e6ad889076a978
waleedtarbosh <waleedtarbush@gmail.com>
2026-07-12 10:29:09 -0400

    Add advanced edge cases: Raw 404, JSON try/catch, Express 404 fallback, and Graceful Shutdown

--- assignment2/sampleHTTP.js
+++ assignment2/sampleHTTP.js
@@ -1,24 +1,22 @@
 const http = require("http");
 
-const htmlString = `
-<!DOCTYPE html>
+const htmlString = `<!DOCTYPE html>
 <html>
 <body>
-<h1>Clock</h1>
-<button id="getTimeBtn">Get the Time</button>
-<p id="time"></p>
-<script>
-document.getElementById('getTimeBtn').addEventListener('click', async () => {
-  const res = await fetch('/time');
-  const timeObj = await res.json();
-  console.log(timeObj);
-  const timeP = document.getElementById('time');
-  timeP.textContent = timeObj.time;
-});
-</script>
+  <h1>Clock</h1>
+  <button id="getTimeBtn">Get the Time</button>
+  <p id="time"></p>
+  <script>
+    document.getElementById('getTimeBtn').addEventListener('click', async () => {
+      const res = await fetch('/time');
+      const timeObj = await res.json();
+      console.log(timeObj);
+      const timeP = document.getElementById('time');
+      timeP.textContent = timeObj.time;
+    });
+  </script>
 </body>
-</html>
-`;
+</html>`;
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === "/time") {
@@ -26,7 +24,7 @@ const server = http.createServer((req, res) => {
     res.end(
       JSON.stringify({
         time: new Date().toString(),
-      }),
+      })
     );
   } else if (req.method === "GET" && req.url === "/timePage") {
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
@@ -39,23 +37,33 @@ const server = http.createServer((req, res) => {
     });
 
     req.on("end", () => {
-      const parsedBody = JSON.parse(body);
-
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(
-        JSON.stringify({
-          weReceived: parsedBody,
-        }),
-      );
+      try {
+        const parsedBody = JSON.parse(body);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(
+          JSON.stringify({
+            weReceived: parsedBody,
+          })
+        );
+      } catch  {
+        res.writeHead(400, { "Content-Type": "application/json" });
+        res.end(
+          JSON.stringify({
+            message: "Invalid JSON.",
+          })
+        );
+      }
     });
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({
         message: "That route is not available.",
-      }),
+      })
     );
   }
 });
 
-server.listen(8000);
+server.listen(8000, () => {
+  console.log("Raw HTTP Server listening on port 8000");
+});
