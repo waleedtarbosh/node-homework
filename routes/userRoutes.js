97d3a2e2a4e6d4f5a134f7f10fcc35ad0e056800
waleedtarbosh <waleedtarbush@gmail.com>
2026-07-19 08:45:08 -0400

    feat: setup user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { register, logon, logoff } = require('../controllers/userController');
+
+router.post('/register', register);
+router.post('/logon', logon);
+router.post('/logoff', logoff);
+
+module.exports = router;
