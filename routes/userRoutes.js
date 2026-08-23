1bf016d9d92da956f3e66bb942a203000cdb031b
waleedtarbosh <waleedtarbush@gmail.com>
2026-08-23 11:52:41 -0400

    feat: protect logoff route with jwt middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { register, logon, logoff } = require('../controllers/userController');
+const jwtMiddleware = require('../middleware/jwtMiddleware');
 
 router.post('/register', register);
 router.post('/logon', logon);
-router.post('/logoff', logoff);
+router.post('/logoff', jwtMiddleware, logoff);
 
 module.exports = router;
